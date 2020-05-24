4a42fc01dba3a0217a682c7595727a8623c61686
Shaun VanWeelden <shaun.vanweelden@scaleapi.com>
2020-05-23 19:11:02 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -21,22 +21,38 @@ app.post('/ping', (req, res) => {
 });
 
 app.post('/send_reminder', (req, res) => {
-  console.log(req.body);
-  res.status(200).send('Pong2');
+  // console.log(req.body);
+
+  const payload = req.body;
+
+  const currentUser =
+    payload.base.collaboratorsById[payload.base.currentUserId];
+
+  const msg = {
+    to: 'shaun.t.vanweelden@gmail.com',
+    from: 'shaun@snooze-bot.com',
+    subject: `[${payload.base.name}] ${
+      currentUser.name.split()[0]
+    } reminded you of X`
+    // templateId: process.env.SENDGRID_TEMPLATE_ON_ERROR,
+    // dynamic_template_data: {
+    //   action_text: 're-open a conversation for you',
+    //   error_text:
+    //     'User is no longer authenticated to SnoozeBot, please re-connect SnoozeBot to Help Scout in Help Scout',
+    //   helpscout_link: `https://secure.helpscout.net/conversation/${snooze.id}/`
+    // }
+  };
+
+  try {
+    sgMail.send(msg);
+    res.status(200).send('Pong2');
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Server hit error sending email');
+  }
 });
 
-// const msg = {
-//   to: 'shaun.t.vanweelden@gmail.com',
-//   from: 'shaun@snooze-bot.com',
-//   templateId: process.env.SENDGRID_TEMPLATE_ON_ERROR,
-//   dynamic_template_data: {
-//     action_text: 're-open a conversation for you',
-//     error_text: 'User is no longer authenticated to SnoozeBot, please re-connect SnoozeBot to Help Scout in Help Scout',
-//     helpscout_link: `https://secure.helpscout.net/conversation/${snooze.id}/`,
-//   },
-// };
-
-// sgMail.send(msg);
+//
 
 // helpscout.addConversationTag("326808", "883799877", "snoozing");
 // helpscout.removeConversationTag("326808", "883799877", "snoozing");
